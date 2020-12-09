7e160912bf4cc063da654037f97f0da4ac540558
mengqianxiang <qx.meng@zuolin.com>
2020-12-09 16:54:33 +0800

    init http project

---
+++ httpservice/src/test/java/com/http/httpservice/HttpserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.http.httpservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HttpserviceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
